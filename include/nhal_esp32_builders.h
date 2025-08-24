b68af4a7c793e0f0861787005affe2d49f55b604
Fo-Zi <facundozingarelli@gmail.com>
2025-08-23 22:34:27 -0300

    Adding helpers for better organization and easier initialization in Platform Layer

---
+++ include/nhal_esp32_builders.h
@@ -0,0 +1,80 @@
+#ifndef NHAL_ESP32_BUILDERS_H
+#define NHAL_ESP32_BUILDERS_H
+
+#include "driver/uart.h"
+
+#define NHAL_ESP32_I2C_MASTER_BUILD(name, bus_id, sda, scl, sda_pullup, scl_pullup, clock_freq) \
+    static struct nhal_i2c_impl_config name##_i2c_impl_cfg = { \
+        .mode = I2C_MODE_MASTER, \
+        .sda_io_num = (sda), \
+        .scl_io_num = (scl), \
+        .sda_pullup_en = (sda_pullup) ? GPIO_PULLUP_ENABLE : GPIO_PULLUP_DISABLE, \
+        .scl_pullup_en = (scl_pullup) ? GPIO_PULLUP_ENABLE : GPIO_PULLUP_DISABLE \
+    }; \
+    static struct nhal_i2c_impl_ctx name##_i2c_impl_ctx = {0}; \
+    static struct nhal_i2c_config name##_i2c_cfg = { \
+        .clock_speed_hz = (clock_freq), \
+        .impl_config = &name##_i2c_impl_cfg \
+    }; \
+    static struct nhal_i2c_context name##_i2c_ctx = { \
+        .i2c_bus_id = (bus_id), \
+        .current_mode = NHAL_I2C_OP_MODE_SYNC_ONLY, \
+        .impl_ctx = &name##_i2c_impl_ctx \
+    };
+
+#define NHAL_ESP32_I2C_CONFIG_REF(name) (&name##_i2c_cfg)
+#define NHAL_ESP32_I2C_CONTEXT_REF(name) (&name##_i2c_ctx)
+
+#define NHAL_ESP32_PIN_BUILD(name, pin_number, dir, pullup_en, pulldown_en, intr ) \
+    static struct nhal_pin_id name##_pin_id = { \
+        .pin_num = (pin_number) \
+    }; \
+    static struct nhal_pin_impl_config name##_pin_impl_cfg = { \
+        .intr_type = (intr) \
+    }; \
+    static struct nhal_pin_impl_ctx name##_pin_impl_ctx = {0}; \
+    static struct nhal_pin_config name##_pin_cfg = { \
+        .direction = (dir), \
+        .pull_mode = (pullup_en) ? ((pulldown_en) ? NHAL_PIN_PMODE_PULL_UP_AND_DOWN : NHAL_PIN_PMODE_PULL_UP) : ((pulldown_en) ? NHAL_PIN_PMODE_PULL_DOWN : NHAL_PIN_PMODE_NONE), \
+        .impl_config = &name##_pin_impl_cfg \
+    }; \
+    static struct nhal_pin_context name##_pin_ctx = { \
+        .pin_id = (&name##_pin_id), \
+        .impl_ctx = &name##_pin_impl_ctx \
+    };
+
+#define NHAL_ESP32_PIN_CONFIG_REF(name) (&name##_pin_cfg)
+#define NHAL_ESP32_PIN_CONTEXT_REF(name) (&name##_pin_ctx)
+
+#define NHAL_ESP32_UART_BASIC_BUILD(name, uart_num, tx_pin, rx_pin, baud_rate) \
+    static struct nhal_uart_impl_config name##_uart_impl_cfg = { \
+        .tx_pin_number = (tx_pin), \
+        .rx_pin_number = (rx_pin), \
+        .tx_buffer_size = 1024, \
+        .rx_buffer_size = 1024, \
+        .rts_pin_number = -1, \
+        .cts_pin_number = -1, \
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE, \
+        .source_clk = UART_SCLK_APB, \
+        .intr_alloc_flags = 0, \
+        .queue_size = 10, \
+        .queue_msg_size = 0 \
+    }; \
+    static struct nhal_uart_impl_ctx name##_uart_impl_ctx = {0}; \
+    static struct nhal_uart_config name##_uart_cfg = { \
+        .baudrate = (baud_rate), \
+        .parity = NHAL_UART_PARITY_NONE, \
+        .stop_bits = NHAL_UART_STOP_BITS_1, \
+        .data_bits = NHAL_UART_DATA_BITS_8, \
+        .impl_config = &name##_uart_impl_cfg \
+    }; \
+    static struct nhal_uart_context name##_uart_ctx = { \
+        .uart_bus_id = (uart_num), \
+        .current_mode = NHAL_UART_OP_MODE_SYNC_ONLY, \
+        .impl_ctx = &name##_uart_impl_ctx \
+    };
+
+#define NHAL_ESP32_UART_CONFIG_REF(name) (&name##_uart_cfg)
+#define NHAL_ESP32_UART_CONTEXT_REF(name) (&name##_uart_ctx)
+
+#endif
---
+++ include/nhal_esp32_helpers.h
@@ -0,0 +1,12 @@
+#ifndef NHAL_ESP32_HELPERS_H
+#define NHAL_ESP32_HELPERS_H
+
+#include "esp_err.h"
+#include "nhal_pin_types.h"
+#include "driver/gpio.h"
+
+nhal_result_t nhal_map_esp_err(esp_err_t esp_err);
+nhal_pin_pull_mode_t esp32_to_nhal_pin_pull_mode(uint8_t pullup_en,uint8_t pulldown_en);
+void nhal_to_esp32_pull_mode(nhal_pin_pull_mode_t pull_mode, gpio_pullup_t *pullup_en, gpio_pulldown_t *pulldown_en);
+
+#endif
